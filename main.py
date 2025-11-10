a03caf6807baab1ae120e4faadd2ff8cf8a0f01c
virnuki <virnuki@gmail.com>
2025-11-10 11:37:12 +0300

    Fix find empty, pattern to cluster

--- main.py
+++ main.py
@@ -16,14 +16,13 @@ def find_empties(n: int, k: float) -> set:
     length = int(n * k)
     diff = length - (n * int(k))
     int_part = int(k)
-    step = ceil(n / (diff + 1))
-    print((n / (diff + 1)), step)
+    step = int(n / (diff + 1))
     adds = n % (diff + 1)
-    i, out = 0, set()
+    i, out = -1, set()
     while diff > 0:
-        i += step * int_part
+        i += step * int_part + 1
         if adds > 0:
-            i += 1
+            i += int_part
             adds -= 1
         out |= {i}
         diff -= 1
@@ -38,10 +37,11 @@ def int_to_cluster(n: int, k: float) -> tuple:
 
 
 def get_cluster(values: tuple, size: int) -> list:
+    values_list = list(values)
+    rd.shuffle(values_list)
     cluster = []
-    rd.shuffle(list(values))
     for i in range(size):
-        cluster.append(values[i * size:(i + 1) * size])
+        cluster.append(values_list[i * size:(i + 1) * size])
     return cluster
 
 
@@ -60,59 +60,31 @@ def fill_new_e_random(new_img: np.ndarray, empties: set) -> None:
     for i in range(len(new_img)):
         for j in range(len(new_img)):
             if i in empties or j in empties:
-                new_img[i, j] = rd.randint(0, 1)
+                # new_img[i, j] = rd.randint(0, 1)
+                new_img[j, i] = 1
 
 
-def interpolate(new_img: np.ndarray, empties: set, size: int) -> None:
-    """
-    НЕРАБОЧИЙ НЕЙРОСЛОП
-    """
-    for empty_row in sorted(empties):
-        # Находим ближайшие непустые строки сверху и снизу
-        top_row = empty_row - 1
-        while top_row >= 0 and top_row in empties:
-            top_row -= 1
-
-        bottom_row = empty_row + 1
-        while bottom_row < size and bottom_row in empties:
-            bottom_row += 1
-        # Интерполяция между найденными строками
-        if bottom_row < size - 1:
-            # Линейная интерполяция
-            alpha = (empty_row - top_row) / (bottom_row - top_row)
-            new_img[empty_row] = (1 - alpha) * new_img[top_row] + alpha * new_img[bottom_row]
-        else:
-            # Копируем нижнюю строку
-            new_img[empty_row] = new_img[top_row]
-
-
-def fill_new_i_interp(new_img: np.ndarray, empties: set) -> None:
-    """
-    Заполняет пустые строки и столбцы с помощью интерполяции соседних значений
-    НАДО ПЕРЕПИСАТЬ ФУНКЦИЮ ИНТЕРПОЛЯЦИИ
-    """
-    if not empties:
-        return
-    size = new_img.shape[0]
-    interpolate(new_img, empties, size)
-    img_transposed = new_img.T
-    interpolate(img_transposed, empties, size)
-    new_img[:, :] = img_transposed.T
+def sum_of_cluster(y0: int, x0: int, i_p: int, new_img: np.ndarray) -> int:
+    count = 0
+    for elem in new_img[y0:y0+i_p, x0:x0+i_p]:
+        for num_elem in elem:
+            count += num_elem
+    return count
 
 
 if __name__ == "__main__":
     # Baboo_256.tiff  Pepper_256.tiff
-    img = cv2.imread(f'images/{input()}', cv2.IMREAD_GRAYSCALE)
+    # img = cv2.imread(f'images/{input()}', cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread(f'images/Baboo_256.tiff', cv2.IMREAD_GRAYSCALE)
     n, k = img.shape[0], float(input())
     new_image = create_empty(n, k)
     empt = find_empties(n, k)
     fill_clusters(img, new_image, empt, n, k)
-    # fill_new_e_random(new_image, empt)
-    fill_new_i_interp(new_image, empt)
+    #fill_new_e_random(img, empt)
     np.savetxt('output.csv', new_image, delimiter=',', fmt='%d')
     new_image = add_bright(new_image)
     cv2.imshow('Old image', img)
     cv2.imshow('New image', new_image)
     cv2.imwrite('output.png', new_image)
     cv2.waitKey(0)
-    cv2.destroyWindow('image')
+    cv2.destroyAllWindows()
